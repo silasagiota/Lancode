6934a0e27415884f63325f06f10bdcf9966a003c
silasagiota <silasagiota@gmail.com>
2026-07-04 14:53:58 -0300

    Add files via upload

---
+++ Lancode1.py
@@ -0,0 +1,33 @@
+#primeira lição python
+
+print("Hello world")
+
+# primeira lição lan code
+
+print("Olá estou aprendendo python")
+
+#segunda lição lan code
+
+nome = input("Qual e seu nome? ")
+idade = input("Qual é a sua idade? ")
+
+#dois jeitos de colocar nome e idade
+
+print(" O seu nome é " + nome)
+print(" Sua idade é " + idade)
+
+#porem este jeito e bem mais facil
+
+print(f"Seu nome é {nome}, e sua idade é {idade} anos")
+
+#para somar ou subtrair colocar o int na frente naoe  atras do input igual fez antes!!!
+
+numero1 = int(input("Digite o primeiro número"))
+numero2 = int(input("Digite o segundo número"))
+
+#nao esquecer variaveis
+
+resultado_soma = numero1 + numero2
+resultado_subtracao = numero1 - numero2
+print(resultado_soma)
+print(resultado_subtracao)
